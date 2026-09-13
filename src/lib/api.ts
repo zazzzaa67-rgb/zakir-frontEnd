e210bf56fdc6c4748f6da1e096ed37236c78286f
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-09-13 20:22:18 +0300

    commit frontEnd changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,7 @@
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api';
+const configuredApiUrl = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api';
+const API_URL = configuredApiUrl.replace(/\/$/, '').endsWith('/api')
+  ? configuredApiUrl.replace(/\/$/, '')
+  : `${configuredApiUrl.replace(/\/$/, '')}/api`;
 
 export type StudentProfile = {
   id: string;
